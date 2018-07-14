a0ac25b135941f5d5a930d9c4364ef9a6a0c50b4
Tan Bui <tan@vaadin.com>
2018-07-14 12:25:06 +0300

    Add language swithcer and binding

--- angular-translate-demo/app.js
+++ angular-translate-demo/app.js
@@ -3,35 +3,33 @@ var translationsEn = {
     PARAGRAPH: 'Srsly!',
     NAMESPACE: {
         PARAGRAPH: 'And it comes with awesome features!'
-    }
+    },
+    BUTTON_LANG_EN: "English",
+    BUTTON_LANG_VI: "Vietnamese"
 };
 var translationsVi = {
     HEADLINE: 'Thật tuyệt vời!',
     PARAGRAPH: 'Tất nhiên!',
     NAMESPACE: {
         PARAGRAPH: 'Cùng với tính năng tuyệt vời!'
-    }
+    },
+    BUTTON_LANG_EN: "Tiếng Anh",
+    BUTTON_LANG_VI: "Tiếng Việt"
 };
 
 
 var app = angular.module('myApp', ['pascalprecht.translate']);
 
 app.config(['$translateProvider', function ($translateProvider) {
-    // add translation table
     $translateProvider
         .translations('en', translationsEn)
         .translations('vi', translationsVi)
-        .preferredLanguage('vi');
+        .preferredLanguage('vi')
+        .fallbackLanguage('en');
 }]);
 
-app.controller('Ctrl', ['$scope', '$translate', function ($scope, $translate) {
-    $translate(['HEADLINE', 'PARAGRAPH', 'NAMESPACE.PARAGRAPH']).then(function (translations) {
-        $scope.headline = translations.HEADLINE;
-        $scope.paragraph = translations.PARAGRAPH;
-        $scope.namespaced_paragraph = translations['NAMESPACE.PARAGRAPH'];
-      }, function (translationIds) {
-        $scope.headline = translationIds.headline;
-        $scope.paragraph = translationIds.paragraph;
-        $scope.namespaced_paragraph = translationIds.namespaced_paragraph;
-      });
+app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+    $scope.changeLanguage = function (langKey) {
+        $translate.use(langKey);
+    };
 }]);
