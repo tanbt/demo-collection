20d53273bb82dc167300aee1c5a8654c44691def
tanbui <trungtanbui@gmail.com>
2020-05-04 22:16:35 +0300

    original app before refactoring

--- refactoring-ebook-practice/test/test.js
+++ refactoring-ebook-practice/test/test.js
@@ -1,18 +1,18 @@
 import 'chai/register-expect.js';
-import {runApp} from '../src/app.js';
+import { runApp } from '../src/app.js';
 
-describe('Invoice calculator', function() {
+describe('Invoice calculator', function () {
 
-  it('should print correct invoice details', function() {
+  it('should print correct invoice details', function () {
     const result = runApp();
     expect(result).to.be.equal(`
-    Statement for BigCo
-      Hamlet: $650.00 (55 seats)
-      As You Like It: $580.00 (35 seats)
-      Othello: $500.00 (40 seats)
-    Amount owed is $1,730.00
-    You earned 47 credits
-    `);
+Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`);
   });
 
 });
