570c47b3f3f587bcc94852b9f24582ac252716c0
tanbui <trungtanbui@gmail.com>
2020-05-09 23:28:04 +0300

    add formatter - render html

--- refactoring-ebook-practice/src/app.js
+++ refactoring-ebook-practice/src/app.js
@@ -6,6 +6,10 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function renderPlainText(data) {
   let result = `\nStatement for ${data.customer}\n`;
 
@@ -16,6 +20,20 @@ function renderPlainText(data) {
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
 };
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}<h1>`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td> <td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount / 100)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is ${usd(data.totalAmount / 100)}</p>`;
+  result += `<p>You earned ${data.totalVolumeCredits} credits</p>`;
+  return result;
+}
 
 function usd(aNumber) {
   return new Intl.NumberFormat('en-US', {
@@ -29,4 +47,8 @@ const renderConsole = () => {
   return statement(getInvoices()[0], getPlays());
 };
 
-export { renderConsole };
+const printHtml = () => {
+  return htmlStatement(getInvoices()[0], getPlays());
+}
+
+export { renderConsole, printHtml };
